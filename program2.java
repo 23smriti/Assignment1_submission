c117a6494424f23a9a9ce632d6f0122aab112f1b
23smriti <83461863+23smriti@users.noreply.github.com>
2021-05-10 19:12:04 +0530

    Create program2.java

---
+++ program2.java
@@ -0,0 +1,30 @@
+import java.util.*;
+  
+  public class Main{
+  
+  public static void main(String[] args) {
+      Scanner scn = new Scanner(System.in);
+       int n=scn.nextInt();
+       int t=n;
+       int count=0;
+        int sum=0;
+       while(n!=0)
+       {
+          n=n/10;
+          count+=1;
+       }
+       n=t;
+       while(n!=0)
+       {
+           int digit=n%10;
+           sum=sum+ (int)Math.pow(digit,count);
+           n=n/10;
+       }
+       
+        
+        if(sum==t)
+        System.out.println("True");
+        else
+        System.out.println("False");
+        }
+  }
